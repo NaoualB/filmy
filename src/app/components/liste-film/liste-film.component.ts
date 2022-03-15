440d27a9c2dc7580e6d5a08974d376d12a82a255
AmandaLan <amanda.lancheong@gmail.com>
2022-03-15 10:33:31 +0100

    list movie card css add

--- src/app/components/liste-film/liste-film.component.ts
+++ src/app/components/liste-film/liste-film.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { MovieService } from 'src/app/services/movie.service';
 
 @Component({
   selector: 'app-liste-film',
@@ -7,9 +8,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListeFilmComponent implements OnInit {
 
-  constructor() { }
+  URL = 'https://api.themoviedb.org/3/discover/movie?api_key=';
+
+  movies: any = [];
+  res : any = {}
+  constructor(private movieService : MovieService) {}
 
   ngOnInit(): void {
+    this.moviesOnGet();
   }
 
+ 
+  moviesOnGet(): void{
+    this.movieService.getMovies().subscribe(
+      (reponse)=>{
+        console.log(reponse);
+        this.res = reponse;
+        this.movies = this.res.results;
+        
+        // this.movies = reponse['results'][1];
+        // this.movies = reponse.results;
+  },
+    )
+  }
 }
